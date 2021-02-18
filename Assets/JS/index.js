55a29f650969c79c0d5d60b60fe41cc99884b626
Jung Lee <koreanstig@gmail.com>
2021-02-17 20:29:41 -0800

    made small changes to css

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,3 +1,7 @@
 $("#githublogo").on('click', function(){
     window.location = "https://github.com/koreanstig";
 })
+
+$("#linkedlogo").on('click', function(){
+    window.location = "https://www.linkedin.com/in/jung-lee1991/";
+})
